cf689aeaf705755e544919a0204b8aa1615f9597
edoardoramalli <edoardo.ramalli@gmail.com>
2020-11-16 14:38:25 +0100

    Allowed hosts

--- SciExpeM/settings.py
+++ SciExpeM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = False
 COMPRESS_ENABLED = True
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
